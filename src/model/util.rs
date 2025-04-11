83fad4fba3755b0d135c28b3f219bf199cdb1989
benjamingreenfield <bsbgreenfield@gmail.com>
2025-04-11 16:46:51 -0700

    add slices of indices and vertices to GMesh instances

--- src/model/util.rs
+++ src/model/util.rs
@@ -1,11 +1,23 @@
 use super::model::Model;
+use gltf::accessor::DataType;
+use gltf::buffer::View;
 use gltf::{Accessor, Gltf, Mesh, Node};
 use std::cell::RefCell;
 use std::collections::HashSet;
+use std::fmt::Debug;
 use std::ops::Deref;
-use std::path::PathBuf;
+use std::path::{self, PathBuf};
 use std::rc::Rc;
 use std::{env, fs};
+
+#[derive(Debug)]
+pub enum GltfErrors {
+    NoIndices,
+    NoView,
+    NoPrimitive,
+    IndicesError(String),
+    VericesError(String),
+}
 
 pub struct NodeWrapper<'a> {
     node_ref: Rc<GNode<'a>>,
@@ -29,18 +41,82 @@ pub struct GMesh<'a> {
 }
 
 impl<'a> GMesh<'a> {
-    pub fn new(maybe_mesh: &Option<Mesh>, buffer_data: &Vec<u8>) -> Option<Self> {
+    pub fn new(
+        maybe_mesh: &Option<Mesh>,
+        buffer_data: &'a Vec<u8>,
+    ) -> Result<Option<Self>, GltfErrors> {
         if let Some(mesh) = maybe_mesh {
             // for now, only accept one primitive per mesh
-            let primitive = mesh.primitives().nth(1);
-            Some(Self {
-                index_slice: &[],
-                vertex_slice: &[],
+            let primitive = mesh.primitives().nth(0).ok_or(GltfErrors::NoPrimitive)?;
+            let indices_accessor = primitive.indices().ok_or(GltfErrors::NoIndices)?;
+            let indices = Self::get_mesh_indices(buffer_data, &indices_accessor)?;
+            let mut maybe_vertices: Option<&[u8]> = None;
+            for (semantic, accessor) in primitive.attributes() {
+                match semantic {
+                    gltf::Semantic::Positions => {
+                        let vertices_result = Self::get_mesh_vertices(buffer_data, &accessor)?;
+                        let _ = maybe_vertices.insert(vertices_result);
+                    }
+                    _ => {}
+                }
+            }
+            let vertices = maybe_vertices.ok_or(GltfErrors::VericesError(String::from(
+                "could not get vertices from the accessor",
+            )))?;
+            Ok(Some(Self {
+                index_slice: indices,
+                vertex_slice: vertices,
                 normal_slice: &[],
-            })
+            }))
         } else {
-            return None;
+            return Ok(None);
         }
+    }
+    fn get_mesh_vertices(
+        buffer_data: &'a Vec<u8>,
+        position_accessor: &Accessor,
+    ) -> Result<&'a [u8], GltfErrors> {
+        if position_accessor.data_type() != DataType::F32 {
+            return Err(GltfErrors::VericesError(String::from(
+                "the data type of the vertices is something other than an F32!!",
+            )));
+        }
+        let vertices_buffer_view = position_accessor.view().ok_or(GltfErrors::NoView)?;
+        match vertices_buffer_view.stride() {
+            Some(_) => todo!("havent implemented stride for bufferview"),
+            None => {
+                return Ok(Self::get_byte_data_from_view(
+                    buffer_data,
+                    &vertices_buffer_view,
+                ));
+            }
+        }
+    }
+    fn get_mesh_indices(
+        buffer_data: &'a Vec<u8>,
+        indices_accessor: &Accessor,
+    ) -> Result<&'a [u8], GltfErrors> {
+        if indices_accessor.data_type() != DataType::U16 {
+            return Err(GltfErrors::IndicesError(String::from(
+                "the data type of this meshes indices is something other than u16!",
+            )));
+        }
+        let indices_buffer_view = indices_accessor.view().ok_or(GltfErrors::NoView)?;
+        match indices_buffer_view.stride() {
+            Some(_) => todo!("havent implemented strides for bufferview yet!!"),
+            None => {
+                return Ok(Self::get_byte_data_from_view(
+                    buffer_data,
+                    &indices_buffer_view,
+                ));
+            }
+        }
+    }
+
+    fn get_byte_data_from_view(buffer_data: &'a Vec<u8>, view: &View) -> &'a [u8] {
+        let start = view.offset();
+        let end = start + view.length();
+        &buffer_data[start..end]
     }
 }
 
@@ -49,7 +125,7 @@ impl<'a> GMesh<'a> {
 // creating the required buffers
 pub struct GModel<'a> {
     byte_data: Rc<Vec<u8>>,
-    node: Option<GNode<'a>>,
+    node: Option<Rc<GNode<'a>>>,
 }
 
 // used to gather the models
@@ -58,18 +134,29 @@ pub struct GScene<'a> {
 }
 
 impl<'a> GScene<'a> {
-    pub fn new(nodes: gltf::iter::Nodes, root_nodes_ids: Vec<usize>, buffer_data: Vec<u8>) -> Self {
+    pub fn new(
+        nodes: gltf::iter::Nodes<'a>,
+        root_nodes_ids: Vec<usize>,
+        buffer_data: &'a Vec<u8>,
+    ) -> Self {
         // step 1: get the nodes
-        let g_nodes = get_nodes(nodes, root_nodes_ids, buffer_data);
-        Self { models: vec![] }
+        let g_nodes = get_nodes(nodes, root_nodes_ids, &buffer_data);
+        let mut models = Vec::with_capacity(g_nodes.len());
+        for node in g_nodes {
+            models.push(GModel {
+                byte_data: Rc::new(vec![]),
+                node: Some(node),
+            });
+        }
+        Self { models }
     }
 }
 
-fn get_nodes(
+fn get_nodes<'a>(
     nodes: gltf::iter::Nodes,
     root_nodes_ids: Vec<usize>,
-    buffer_data: Vec<u8>,
-) -> Vec<Rc<GNode>> {
+    buffer_data: &'a Vec<u8>,
+) -> Vec<Rc<GNode<'a>>> {
     let mut node_wrappers = Vec::<NodeWrapper>::with_capacity(nodes.len());
     let mut ret: Vec<Rc<GNode>> = Vec::with_capacity(root_nodes_ids.len());
     for node in nodes {
@@ -79,7 +166,7 @@ fn get_nodes(
         // in this first pass process all node data besides the children to ensure that
         // everything actually exists before recursing.
         let node = GNode {
-            mesh,
+            mesh: mesh.unwrap(),
             transform,
             children: RefCell::new(Vec::with_capacity(children.len())),
         };
@@ -90,12 +177,12 @@ fn get_nodes(
         });
         // for each root node, loop through the children at the indices specified for this node
         // clone a new Rc from the Rc<child> node and add it to the children vec
-        for i in &root_nodes_ids {
-            let root_node = &node_wrappers[*i].node_ref.clone();
-            let child_indices = &node_wrappers[*i].child_indices;
-            build_node(&node_wrappers, root_node, child_indices);
-            ret.push(root_node.clone());
-        }
+    }
+    for i in &root_nodes_ids {
+        let root_node = &node_wrappers[*i].node_ref.clone();
+        let child_indices = &node_wrappers[*i].child_indices;
+        build_node(&node_wrappers, root_node, child_indices);
+        ret.push(root_node.clone());
     }
     ret
 }
@@ -107,15 +194,11 @@ fn build_node<'a>(
 ) {
     for child_id in child_indices {
         let child_wrapper = &g_nodes[*child_id];
+        let child_child_indices = &child_wrapper.child_indices;
         let child = child_wrapper.node_ref.clone();
-        build_node(g_nodes, &child, child_indices);
+        build_node(g_nodes, &child, child_child_indices);
         root_node.add_child(child);
     }
-}
-
-pub enum gltf_errors {
-    NoIndices,
-    NoView,
 }
 
 pub fn load_gltf(dirname: &str) -> Result<Vec<Model>, gltf::Error> {
@@ -130,13 +213,12 @@ pub fn load_gltf(dirname: &str) -> Result<Vec<Model>, gltf::Error> {
     let buffer_data = std::fs::read(bin)?;
     // only use the first scene for now
     let scene = gltf.scenes().next().ok_or(gltf::Error::UnsupportedScheme)?;
-    let models = Vec::<Model>::with_capacity(gltf.nodes().len());
     let root_nodes = scene.nodes().map(|n| n.index()).collect();
-    let scene: GScene = GScene::new(gltf.nodes(), root_nodes, buffer_data);
+    let scene: GScene = GScene::new(gltf.nodes(), root_nodes, &buffer_data);
     Ok(vec![])
 }
 
-fn get_node_meshes(node: Node) -> Result<Vec<Mesh>, gltf_errors> {
+fn get_node_meshes(node: Node) -> Result<Vec<Mesh>, GltfErrors> {
     let mut meshes = Vec::<Mesh>::new();
     if let Some(mesh) = node.mesh() {
         meshes.push(mesh);
@@ -151,13 +233,13 @@ fn get_node_meshes(node: Node) -> Result<Vec<Mesh>, gltf_errors> {
     Ok(meshes)
 }
 
-fn get_indices_slice<'a>(mesh: &'a Mesh) -> Result<&'a [f32], gltf_errors> {
+fn get_indices_slice<'a>(mesh: &'a Mesh) -> Result<&'a [f32], GltfErrors> {
     // TODO: process more primitives?
     // use only the first primitive for now
     let primitive = mesh.primitives().next().unwrap();
 
     // if there are no indices specified for this primitive, thats an error
-    let indices_accessor: Accessor = primitive.indices().ok_or(gltf_errors::NoIndices)?;
+    let indices_accessor: Accessor = primitive.indices().ok_or(GltfErrors::NoIndices)?;
     Ok(&[])
 }
 
@@ -178,20 +260,23 @@ fn get_gltf_file(dir_path: PathBuf) -> Result<(PathBuf, PathBuf), std::io::Error
         }
         if let Ok(e) = entry {
             let path = e.path();
-            match path.extension().ok_or(err)?.to_str().ok_or(err)? {
-                "gltf" => {
-                    if gltf_file.is_some() {
-                        return Err(err.into());
+            match path.extension() {
+                Some(path_ext) => match path_ext.to_str().ok_or(err)? {
+                    "gltf" => {
+                        if gltf_file.is_some() {
+                            return Err(err.into());
+                        }
+                        gltf_file = Some(path);
                     }
-                    gltf_file = Some(path);
-                }
-                "bin" => {
-                    if bin_file.is_some() {
-                        return Err(err.into());
+                    "bin" => {
+                        if bin_file.is_some() {
+                            return Err(err.into());
+                        }
+                        bin_file = Some(path);
                     }
-                    bin_file = Some(path);
-                }
-                _ => {}
+                    _ => {}
+                },
+                None => {}
             }
         }
     }
@@ -199,5 +284,17 @@ fn get_gltf_file(dir_path: PathBuf) -> Result<(PathBuf, PathBuf), std::io::Error
         return Err(err.into());
     } else {
         return Ok((gltf_file.unwrap(), bin_file.unwrap()));
+    }
+}
+fn print_model<'a>(model: &GModel<'a>) {
+    if let Some(root_node) = &model.node {
+        print_node(root_node);
+    }
+}
+fn print_node(node: &GNode) {
+    println!("node with {} children", node.children.borrow().len());
+    for (idx, child) in node.children.borrow().iter().enumerate() {
+        println!("child {}", idx);
+        print_node(child);
     }
 }
